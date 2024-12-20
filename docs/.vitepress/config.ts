468311c379ed752991eb5379eafd08dd8893942a
ProbirSarkar001 <probir.sarkar.agt@gmail.com>
2024-12-20 13:28:40 +0530

    remove cache

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -63,7 +63,7 @@ export default defineConfig({
     ],
     footer: {
       message: 'Released under the MIT License.',
-      copyright: 'Copyright © 2023'
+      copyright: 'Copyright © 2024'
     }
   }
 })
